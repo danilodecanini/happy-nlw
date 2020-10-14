e2f8f3df489107b6c0da694eb04dcd1c4b3d7b01
Danilo Henrique de Canini Romani <daniloh@romagnole.com.br>
2020-10-14 09:37:31 -0300

    🎉 feat: add Orphanage Detail and Create Orphanage pages

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
 import Landing from './pages/Landing';
 import OrphanagesMap from './pages/OrphanagesMap';
+import Orphanage from './pages/Orphanage';
+import CreateOrphanage from './pages/CreateOrphanage';
 
 function Routes() {
   return (
@@ -9,6 +12,9 @@ function Routes() {
       <Switch>
         <Route path="/" exact component={Landing} />
         <Route path="/app" component={OrphanagesMap} />
+
+        <Route path="/orphanages/create" component={CreateOrphanage} />
+        <Route path="/orphanages/:id" component={Orphanage} />
       </Switch>
     </BrowserRouter>
   )
