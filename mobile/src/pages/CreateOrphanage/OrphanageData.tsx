5120694a08b15b26098b985de9159c2e30d2308f
Danilo de Canini <danilodecanini@gmail.com>
2020-10-16 20:44:43 -0300

    ⚡ fix: throw an error when select image

--- mobile/src/pages/CreateOrphanage/OrphanageData.tsx
+++ mobile/src/pages/CreateOrphanage/OrphanageData.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { ScrollView, View, StyleSheet, Switch, Text, TextInput, TouchableOpacity } from 'react-native';
+import { ScrollView, View, StyleSheet, Switch, Text, TextInput, TouchableOpacity, Image } from 'react-native';
 import { Feather } from '@expo/vector-icons';
 import { RectButton } from 'react-native-gesture-handler';
 import { useNavigation, useRoute } from '@react-navigation/native';
@@ -99,7 +99,6 @@ export default function OrphanageData() {
       /> */}
 
       <Text style={styles.label}>Fotos</Text>
-
       <View style={styles.uploadedImagesContainer}>
         {images.map(image => {
           return (
